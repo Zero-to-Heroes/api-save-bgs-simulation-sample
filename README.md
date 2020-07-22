ee96ebf6ea381e17dd482c2cfe2683a7c61de6c7
sebastientromp <sebastien.tromp@gmail.com>
2020-07-22 11:53:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Test it
+
+```
+sam local start-api
+```
+
+# Deploy
+
+```
+npm run build && npm run package && npm run deploy
+```
+
+# Reference
+
+Used this project as template: https://github.com/alukach/aws-sam-typescript-boilerplate
---
+++ src/bgs-get-id-from-sample.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import { getConnection as getConnectionBgs } from './services/rds-bgs';
+import { encode } from './services/utils';
+
+// This example demonstrates a NodeJS 8.10 async handler[1], however of course you could use
+// the more traditional callback-style handler.
+// [1]: https://aws.amazon.com/blogs/compute/node-js-8-10-runtime-now-available-in-aws-lambda/
+export default async (event): Promise<any> => {
+	try {
+		// console.log('processing event', event);
+		const sample: any = event.body;
+		// console.log('sample is', sample);
+		const encoded = encode(event.body);
+
+		const mysqlBgs = await getConnectionBgs();
+
+		// Check if this sample already exists in db
+		const dbResults: any[] = await mysqlBgs.query(
+			`
+				SELECT id FROM bgs_simulation_samples
+				WHERE sample = '${encoded}'
+			`,
+		);
+
+		if (dbResults && dbResults.length > 0) {
+			// console.log('found existing sample, returning id', dbResults[0]);
+			return {
+				statusCode: 200,
+				body: JSON.stringify(dbResults[0]),
+			};
+		}
+
+		const insertionResult = await mysqlBgs.query(
+			`
+				INSERT INTO bgs_simulation_samples (sample)
+				VALUES ('${encoded}')
+			`,
+		);
+		// console.log('inserted', insertionResult);
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify(insertionResult[0].id),
+		};
+		// console.log('sending back success reponse');
+		return response;
+	} catch (e) {
+		console.error('issue saving sample', e);
+		const response = {
+			statusCode: 500,
+			isBase64Encoded: false,
+			body: null,
+		};
+		console.log('sending back error reponse', response);
+		return response;
+	}
+};
